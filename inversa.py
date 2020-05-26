5f170de867d5f3b99d05f3f086541325d141e0c0
martinbarez <martinbarez@gmail.com>
2020-05-26 21:45:57 +0200

    Con dos desplazamientos me vale, encima bien hechos. Lástima que se haya reformateado todo por alguna razón

--- inversa.py
+++ inversa.py
@@ -1,4 +1,4 @@
-import numpy
+import numpy as np
 import logging
 import sys
 
@@ -6,30 +6,33 @@ from manipular import contar
 
 logging.basicConfig(stream=sys.stderr, level=logging.WARNING)
 
-def inversa(cov, cov_in, cov_up, cov_bc, cov_dg, inv_in, inv_up, inv_bc, inv_dg):
+
+def inversa(cov, cov_in, inv_in):
     n_bandas = cov.shape[0]
-    inv = numpy.zeros([n_bandas, n_bandas], cov.dtype)
+    inv = np.zeros([n_bandas, n_bandas], cov.dtype)
 
     c = contar()
 
     #lo inicializo como identidad
     for i in range(n_bandas):
-        inv[i][i]=1
+        inv[i][i] = 1
 
-    logging.info("Covarianza:")
-    logging.info(cov)
+    cov = cov // 1
+    inv = inv // 1
     cov = cov * pow(2, cov_in)
     inv = inv * pow(2, inv_in)
-    c.truncar(cov)
     logging.info("Covarianza:")
-    logging.info(cov)
+    logging.info(c.truncar(cov))
+    logging.debug(cov)
+    logging.info("Inversa:")
+    logging.info(c.truncar(inv))
 
     #forward elimination to build an upper triangular matrix
-    for i in range (n_bandas):
+    for i in range(n_bandas):
         # if zero
-        if (cov[i][i]==0):
+        if (cov[i][i] == 0):
             for j in range(i, n_bandas):
-                if (cov[j][j]!=0):
+                if (cov[j][j] != 0):
                     cov[[i, j]] = cov[[j, i]]
                     inv[[i, j]] = inv[[j, i]]
                     print("swapped", i, j)
@@ -39,20 +42,12 @@ def inversa(cov, cov_in, cov_up, cov_bc, cov_dg, inv_in, inv_up, inv_bc, inv_dg)
             inv[j] = (inv[j] - inv[i] * (cov[j][i] / cov[i][i]))
             cov[j] = (cov[j] - cov[i] * (cov[j][i] / cov[i][i]))
 
-    logging.info("")
+    inv = inv // 1
     logging.info("Covarianza:")
-    logging.info(cov)
+    logging.info(c.truncar(cov))
     logging.info("Inversa:")
-    logging.info(inv)
-    cov = cov * pow(2, cov_up)
-    inv = inv * pow(2, inv_up)
-    c.truncar(cov)
-    c.truncar(inv)
-    logging.info("Covarianza:")
-    logging.info(cov)
-    logging.info("Inversa:")
-    logging.info(inv)
-
+    logging.info(c.truncar(inv))
+    logging.debug(inv)
 
     #backward elimination to build a diagonal matrix
     for i in range(n_bandas-1, 0, -1):
@@ -60,31 +55,17 @@ def inversa(cov, cov_in, cov_up, cov_bc, cov_dg, inv_in, inv_up, inv_bc, inv_dg)
             inv[j] = (inv[j] - inv[i] * (cov[j][i] / cov[i][i]))
             cov[j] = (cov[j] - cov[i] * (cov[j][i] / cov[i][i]))
 
-    logging.info("")
+    inv = inv // 1
     logging.info("Covarianza:")
-    logging.info(cov)
+    logging.info(c.truncar(cov))
     logging.info("Inversa:")
-    logging.info(inv)
-    cov = cov * pow(2, cov_bc)
-    inv = inv * pow(2, inv_bc)
-    c.truncar(cov)
-    c.truncar(inv)
-    logging.info("Covarianza:")
-    logging.info(cov)
-    logging.info("Inversa:")
-    logging.info(inv)
-
+    logging.info(c.truncar(inv))
+    logging.debug(inv)
 
     #last division to build identity [i][i]/[i][i]
     for i in range(n_bandas):
         inv[i] = (inv[i] * (1 / cov[i][i]))
 
-    logging.info("")
-    logging.info("Inversa:")
-    logging.info(inv)
-    inv = inv * pow(2, inv_dg*cov_dg)
-    c.truncar(inv)
-    logging.info("Inversa:")
-    logging.info(inv)
+    inv = inv // 1
 
     return inv, c.bits
