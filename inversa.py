790cab0a3e74dbe790e7325b73d14e551d3539a0
martinbarez <martinbarez@gmail.com>
2020-05-29 12:07:55 +0200

    Algún día haré algo bien del todo?

--- inversa.py
+++ inversa.py
@@ -16,8 +16,8 @@ def inversa(cov, cov_in, inv_in, div_up, div_bc, div_dg, count_en):
     for i in range(n_bandas):
         inv[i][i] = 1
 
-    cov = cov * pow(2, cov_in)
-    inv = inv * pow(2, inv_in)
+    cov = cov * pow(2, cov_in) //1
+    inv = inv * pow(2, inv_in) //1
     logging.info("Covarianza:")
     c = contar(cov, count_en)
     max_bits = max(max_bits, c)
@@ -44,8 +44,8 @@ def inversa(cov, cov_in, inv_in, div_up, div_bc, div_dg, count_en):
             div = cov[j][i] / cov[i][i]
             div = div*pow(2, div_up)
             div = div // 1
-            inv[j] = inv[j] - inv[i] * div * pow(2, -div_up)
-            cov[j] = cov[j] - cov[i] * div * pow(2, -div_up)
+            inv[j] = inv[j] - inv[i] * div * pow(2, -div_up) //1
+            cov[j] = cov[j] - cov[i] * div * pow(2, -div_up) //1
 
     logging.info("Covarianza:")
     c = contar(cov, count_en)
@@ -64,8 +64,8 @@ def inversa(cov, cov_in, inv_in, div_up, div_bc, div_dg, count_en):
             div = cov[j][i] / cov[i][i]
             div = div*pow(2, div_bc)
             div = div // 1
-            inv[j] = inv[j] - inv[i] * div * pow(2, -div_bc)
-            cov[j] = cov[j] - cov[i] * div * pow(2, -div_bc)
+            inv[j] = inv[j] - inv[i] * div * pow(2, -div_bc) //1
+            cov[j] = cov[j] - cov[i] * div * pow(2, -div_bc) //1
 
     logging.info("Covarianza:")
     c = contar(cov, count_en)
@@ -80,9 +80,8 @@ def inversa(cov, cov_in, inv_in, div_up, div_bc, div_dg, count_en):
 
     #last division to build identity [i][i]/[i][i]
     for i in range(n_bandas):
-        inv[i] = (inv[i] / cov[i][i])* pow(2, div_dg)
+        inv[i] = (inv[i] / cov[i][i])* pow(2, div_dg) //1
 
-    inv = inv // 1
     max_bits = max(max_bits, contar(inv, count_en))
 
     return inv, max_bits
