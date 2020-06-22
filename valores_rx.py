8c7b32a3dc5f46f194f091f33e3349f8d2e039a9
martinbarez <martinbarez@gmail.com>
2020-06-22 13:32:05 +0200

    Updated the rx step to its "final" form, with 25x35 mults

--- valores_rx.py
+++ valores_rx.py
@@ -2,7 +2,7 @@ import numpy as np
 import logging
 import sys
 
-from manipular import contar, contar_num
+from manipular import contar, shift, clamp
 
 
 def valores_rx(n_pixeles, inversa, deviacion, dev_up=0, inter_up=0):
@@ -18,17 +18,17 @@ def valores_rx(n_pixeles, inversa, deviacion, dev_up=0, inter_up=0):
             rx[i] = np.matmul(inter_res, resta)
 
     elif inversa.dtype == np.int64 or inversa.dtype == np.int32:
-        deviacion = deviacion * pow(2, dev_up)
-        deviacion = deviacion // 1
+        deviacion = shift(deviacion, dev_up)
+        deviacion = clamp(deviacion, 25)
+        inversa = clamp(inversa, 35)
         for i in range(n_pixeles):
             resta = deviacion[i].astype(inversa.dtype)
             inter_res = np.zeros(n_bandas, inversa.dtype)
             for j in range(n_bandas):
-                mult = resta[j] * inversa[j]
-                inter_res += mult 
-            # this leaves inter_res with 35 significant bits
-            inter_res = inter_res * pow(2, inter_up)
-            inter_res = inter_res // pow(2, 64 - 35)
+                mult = inversa[j] * resta[j]
+                inter_res += mult
+            inter_res = shift(inter_res, inter_up)
+            inter_res = clamp(inter_res, 35)
             sum = 0
             for j in range(n_bandas):
                 sum += inter_res[j] * resta[j]
