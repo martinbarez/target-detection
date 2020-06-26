3e7714f3ef1015fbd7c6a75350026e1a2b8c4bc4
martinbarez <martinbarez@gmail.com>
2020-06-26 15:04:32 +0200

    Begin optimizing all vector sizes

---
+++ rx_package.py
@@ -0,0 +1,29 @@
+precision        = 24
+ram_precision    = 35
+sorter_precision = 13
+
+dividend_precision    = 64
+divisor_precision     = 64
+quotient_precision    = 64
+gauss_mul_a_precision = 35
+gauss_mul_b_precision = 42
+gauss_mul_p_precision = 42
+gauss_sub_a_precision = 48
+gauss_sub_b_precision = 48
+gauss_sub_s_precision = 48
+
+mean_sub_a_precision = 48
+mean_sub_b_precision = 48
+mean_sub_s_precision = 48
+
+mult_st_mul_a_precision = 35
+mult_st_mul_b_precision = 42
+mult_st_mul_p_precision = 42
+mult_nd_mul_a_precision = precision
+mult_nd_mul_b_precision = 42
+mult_nd_mul_p_precision = 42
+
+mult_st_accum_in_precision  = mult_st_mul_p_precision
+mult_st_accum_out_precision = 48
+mult_nd_accum_in_precision  = mult_nd_mul_p_precision
+mult_nd_accum_out_precision = sorter_precision
