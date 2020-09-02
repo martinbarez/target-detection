Fix small error where the fifo covariance was used to calc the reference and then everything was different so I had to recalc all shift values

operaciones.py
@@ -11,8 +11,7 @@ def deviacion(X, media_bandas):
     Xt = np.transpose(X.copy())
 
     #A cada toma le resto la media de su banda
-    deviacion = (clamp(Xt, rx.mean_sub_a_precision)-clamp(media_bandas, rx.mean_sub_b_precision))
-    return clamp(deviacion, rx.mean_sub_s_precision)
+    return Xt-media_bandas
 
 
 def covarianza(X, deviacion):

todo.py
@@ -1,6 +1,6 @@
 import spectral.io.envi as envi
 import numpy as np
-from manipular import clamp
+from manipular import clamp, shift
 from operaciones import media, deviacion, covarianza
 from inversa import inversa
 from valores_rx import valores_rx
@@ -8,39 +8,46 @@ from comparar import *
 from testbench import gen_testbench
 import rx_package as rx
 
+
 img = envi.open('hydice.hdr').load()
-#para cortar la imagen
-#img = img[10:20, 10:20, 0:55]
+
+#rearrange so each band is a row
 X = np.reshape(img, (img.shape[0] * img.shape[1], img.shape[2]))
-X = np.transpose(X) #para que cada banda sea una fila
+X = np.transpose(X)
+X = X.astype(np.float64)
+
 N = img.shape[2]
 M = img.shape[0] * img.shape[1]
 
-#Lo transformo a float para realizar todas las operaciones
-X = X.astype(np.float64)
+#Calc the reference in floating point
+inverse_reference = np.linalg.inv(np.cov(X))
+reference_rx = valores_rx(X.shape[1], inverse_reference, deviacion(X, media(X)))
 
-#Las operaciones previas
-m_aux = media(X)
-d_aux = deviacion(X, m_aux)
-c_aux = covarianza(X, d_aux)
+#Calc the data how it is going to get written on the FIFOS
+scale = 12
+mean_fifo = clamp(shift(media(X), scale), rx.precision)
+elements_fifo = clamp(shift(X, scale), rx.precision)
+covariance_fifo = clamp(shift(np.cov(X), rx.read_cov), rx.precision)
 
-c_aux = clamp(c_aux, rx.precision)
+#Simulate the FPGA
+mean_fifo = clamp(mean_fifo, rx.mean_sub_a_precision)
+elements_fifo = clamp(elements_fifo, rx.mean_sub_b_precision)
+deviation_fpga = clamp(deviacion(elements_fifo, mean_fifo), rx.mean_sub_s_precision)
 
-#Calculo en flotante para poder comparar luego
-inversa_flotante = np.linalg.inv(c_aux)
-rx_flotante = valores_rx(X.shape[1], inversa_flotante, d_aux)
+target_inverse, bits = inversa(covariance_fifo, False)
+target_rx = valores_rx(X.shape[1], target_inverse, deviation_fpga)
 
-inversa_fija, bits = inversa(c_aux, False)
-rx_fijo = valores_rx(X.shape[1], inversa_fija, d_aux)
+#Analyze results
+target_res, reference_res = coords(target_rx, reference_rx, 30)
 
-res_ordenados = coords(rx_fijo, rx_flotante, 30)
+from difflib import SequenceMatcher
+print(SequenceMatcher(None, target_res, reference_res).ratio())
 
-
-if True:
+if False:
     from matplotlib import pyplot as plt
-    simple_results = analyze(X, res_ordenados[0], res_ordenados[1], simple_compare, -1)
-    spatial_results = analyze(X, res_ordenados[0], res_ordenados[1], spatial_compare, 1)
-    angle_results = analyze(X, res_ordenados[0], res_ordenados[1], angle_compare, 3)
+    simple_results = analyze(X, target_res, reference_res, simple_compare, -1)
+    spatial_results = analyze(X, target_res, reference_res, spatial_compare, 1)
+    angle_results = analyze(X, target_res, reference_res, angle_compare, 3)
 
     simple_ratios = []
     spatial_ratios = []
@@ -60,7 +67,7 @@ if True:
 
     plt.xlabel("Number of elements")
     plt.ylabel("Ratio between found/total")
-    plt.axis([0, len(res_ordenados[0]), 0, 1.1])
+    plt.axis([0, len(target_res), 0, 1.1])
 
     plt.plot(simple_ratios, 'r--')
     plt.plot(spatial_ratios, 'g-.')
@@ -71,11 +78,11 @@ if True:
 if False:
     with open('results_hydice_truth.txt', 'w') as file:
         for i in range(0, M):
-            file.write(str(res_ordenados[1][i])+'\n')
+            file.write(str(reference_res[i])+'\n')
 
     with open('results_hydice_simul.txt', 'w') as file:
         for i in range(0, M):
-            file.write(str(res_ordenados[0][i])+'\n')
+            file.write(str(target_res[i])+'\n')
 
 if False:
-    gen_testbench(m_aux, c_aux, X, res_ordenados[1])
+    gen_testbench(mean_fifo, covariance_fifo, X, target_res)